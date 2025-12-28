90b67218b0db97edccd328142c1c44e013e28023
Maksym <1maksymrusanov1@gmail.com>
2025-12-28 18:32:07 +0000

    change nvim config(fix telescope)

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,21 +4,29 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    -- рекомендую убрать тег, чтобы ставилась последняя версия
+    -- tag = "0.1.5", -- <--- лучше удалить для обновления
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          preview = {
+            treesitter = false, -- отключаем Treesitter в предпросмотре
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
       })
+
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = 'Telescope diagnostics' })
+      vim.keymap.set("n", "<leader>td", builtin.diagnostics, { desc = "Telescope diagnostics" })
     end,
   },
 }
+
