f86779862171fef7e6471fcec7460c3beeb2da94
Maksym <1maksymrusanov1@gmail.com>
2026-04-13 21:55:16 +0100

    fix:change hotkey for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
 
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader>td", builtin.diagnostics, { desc = "Telescope diagnostics" })
 		end,
