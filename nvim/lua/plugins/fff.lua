766c0e3713e5f197525d44c94a8bf1e2a54d8cbb
Maksym <1maksymrusanov1@gmail.com>
2026-04-28 19:45:03 +0100

    fix: add new plugin(fff)

---
+++ nvim/lua/plugins/fff.lua
@@ -0,0 +1,32 @@
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = function()
+		-- downloads a prebuilt binary or falls back to cargo build
+		require("fff.download").download_or_build_binary()
+	end,
+	-- for nixos:
+	-- build = "nix run .#release",
+	opts = {
+		debug = {
+			enabled = true,
+			show_scores = true,
+		},
+	},
+	lazy = false, -- the plugin lazy-initialises itself
+	keys = {
+		{
+			"<C-p>",
+			function()
+				require("fff").find_files()
+			end,
+			desc = "FFFind files",
+		},
+		{
+			"<leader><leader>",
+			function()
+				require("fff").live_grep()
+			end,
+			desc = "LiFFFe grep",
+		},
+	},
+}
